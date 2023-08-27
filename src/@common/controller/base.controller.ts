4d3df0cec9927e8cde5621e91f13955858d71ff6
Aleksandr Schemelev <aleksandr.schemelev>
2023-08-27 20:33:23 +0300

    feat: add BaseController

---
+++ src/@common/controller/base.controller.ts
@@ -0,0 +1,44 @@
+import { Response, Router } from 'express';
+import { LoggerService }    from '../../logger/logger';
+import { Route }            from './route.interface';
+
+export enum HttpStatus {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500,
+}
+
+export abstract class BaseController {
+  private readonly _router: Router;
+
+  public constructor(private readonly logger: LoggerService) {
+    this._router = Router();
+  }
+
+  public get router(): Router {
+    return this._router;
+  }
+
+  public send<T>(res: Response, status: number, data: T) {
+    return res.status(status).type('application/json').send(data);
+  }
+
+  public ok<T>(res: Response, data: T) {
+    return this.send(res, HttpStatus.OK, data);
+  }
+
+  public created<T>(res: Response, data: T) {
+    return this.send(res, HttpStatus.CREATED, data);
+  }
+
+  protected bindRoutes(routes: Route[]) {
+    for (const route of routes) {
+      this.logger.log(`[${route.method}] ${route.path}`);
+      this.router[route.method](route.path, route.func.bind(this));
+    }
+  }
+}
